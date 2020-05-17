26575c9f73d29a9069aec98f31396f80c5ca3cf3
JasonSarrio <jsarrio16@gmail.com>
2020-05-17 18:38:09 -0500

    reworking code

--- client/src/components/Bubbles.js
+++ client/src/components/Bubbles.js
@@ -14,40 +14,4 @@ const Bubbles = ({ colors }) => {
 
   return (
     <div className="bubble-wrap">
-      <p>bubbles</p>
-      <Svg width={400} height={400}>
-        <Pack
-          data={{
-            children: bubbleData
-          }}
-          sum={datum => datum.value}
-          size={[400, 400]}
-          includeRoot={false}
-          nodeEnter={d => ({ ...d, r: 0 })}
-          animate
-        >
-          {nodes =>
-            nodes
-              .map(({ x, y, r, key }, i) => {
-                if (i < colors.length) {
-                  return (
-                    <Circle
-                      key={key}
-                      cx={x}
-                      cy={y}
-                      r={r}
-                      fill={colors[i].code.hex}
-                    />
-                  );
-                }
-                return null;
-              })
-              .filter(v => v)
-          }
-        </Pack>
-      </Svg>
-    </div>
-  );
-};
-
-export default Bubbles;
+  
